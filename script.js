93522ea67bc00d58d73be9c0781fe865e7282c3f
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-08-10 12:21:25 +0700

    Update and rename login.js to script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// Toggle password visibility
+const passwordInput = document.getElementById("pwd");
+const eyeIcon = document.getElementById("eye");
+
+eyeIcon.addEventListener("click", function() {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+  }
+});
+
+// Handle login form submission
+document.getElementById("login-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const username = document.getElementById("txt-input").value;
+  const password = passwordInput.value;
+
+  // Here you can perform your login validation logic using AJAX or fetch requests.
+  // For demonstration purposes, let's just display a message.
+  
+  if (username === "demo" && password === "password") {
+    // Redirect to the welcome page on successful login
+    window.location.href = "welcome.html";
+  } else {
+    alert("Invalid credentials. Please try again.");
+  }
+});
