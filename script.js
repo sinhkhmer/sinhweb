9883ac4ba455bc0778023a8346631536e2cd9ade
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-08-22 21:22:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-document.getElementById("login").addEventListener("click", function() {
-      const username = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
+// script.js
+const loginForm = document.getElementById('login-form');
 
-      // Replace these values with your actual login credentials
-      const validUsername = "admin";
-      const validPassword = "26166314";
-
-      if (username === validUsername && password === validPassword) {
-        // Redirect to the welcome page on successful login
-        window.location.href = "welcome.html";
-      } else {
-        alert("ឈ្មោះអ្នកប្រើប្រាស់ ឬ ពាក្យសម្ងាត់មិនត្រឹមត្រូវ។ សូមព្យាយាមម្តងទៀត។");
-      }
-    });
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    // Perform your login validation here
+    if (username === 'sinh' && password === 'sinh') {
+                // Redirect to the welcome page on successful login
+        window.location.href = 'welcome.html';
+    } else {
+        alert('Invalid credentials. Please try again.');
+    }
+});
